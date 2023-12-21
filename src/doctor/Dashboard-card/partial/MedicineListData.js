22e33c47f966d0c55e97e034daa53029c8fc6285
Roshan Deorukhkar <roshandeorukhkar@gmail.com>
2023-12-21 19:43:52 +0530

    shubhu add upload csv file functions

--- src/doctor/Dashboard-card/partial/MedicineListData.js
+++ src/doctor/Dashboard-card/partial/MedicineListData.js
@@ -1,28 +1,16 @@
 import React, { useEffect, useState } from 'react';
-import Papa from 'papaparse'
-import { makeStyles } from '@material-ui/core/styles';
-import { getStorage, ref, uploadBytesResumable, getDownloadURL, uploadBytes } from "firebase/storage";
-import ReportApi from '../../../services/ReportApi';
 import { MainButtonInput } from '../../../mainComponent/mainButtonInput';
-import { toast } from "react-toastify";
 import axios from 'axios';
 import { API } from '../../../config';
 
 export default function MedicineListData(props) {
-    const { medicineId, doctorId } = props
-
-    const { saveMedicineList, getMedicineList } = ReportApi()
-    const [saveMedicine, setSaveMedicine] = useState([])
-    const [medicineList, setMedicineList] = useState()
+    const { medicineId } = props
     const [getCSV, setCSV] = useState("")
-    console.log("getCSV-----------", getCSV)
     const saveData = async(e) => {
         e.preventDefault();
         const data = new FormData();
         data.append("file", getCSV);
         data.append('medicines_code', medicineId);
-        console.log("data-----------", data)
-
         await axios({
             method: "POST",
             url: `${API}/add_mymedicines_list`,
@@ -42,7 +30,6 @@ export default function MedicineListData(props) {
                 className='add_bottom_15'
                 required
             />
-
             <MainButtonInput> Save</MainButtonInput>
         </form>
     )
